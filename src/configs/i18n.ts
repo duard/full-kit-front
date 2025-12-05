2dedcc2ac5618384cbaceb24b71ce5d18c28ea25
Carlos Developer <duard.developer@gmail.com>
2025-12-05 06:29:27 -0300

    pnpmi; pnpm dev

--- src/configs/i18n.ts
+++ src/configs/i18n.ts
@@ -1,12 +1,14 @@
 export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "ar"],
+  defaultLocale: "pt-BR",
+  locales: ["en", "ar", "pt-BR"],
   localeDirection: {
     en: "ltr",
     ar: "rtl",
+    "pt-BR": "ltr",
   },
   localeNames: {
     en: "english",
     ar: "arabic",
+    "pt-BR": "portuguese",
   },
 } as const
--- src/lib/get-dictionary.ts
+++ src/lib/get-dictionary.ts
@@ -8,6 +8,8 @@ const dictionaries = {
     import("@/data/dictionaries/en.json").then((module) => module.default),
   ar: () =>
     import("@/data/dictionaries/ar.json").then((module) => module.default),
+  "pt-BR": () =>
+    import("@/data/dictionaries/pt-BR.json").then((module) => module.default),
 }
 
 export async function getDictionary(locale: LocaleType) {
